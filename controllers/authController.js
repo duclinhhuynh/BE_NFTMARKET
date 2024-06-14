0e76f6a29943891e4816ec93057ee6c06dc8a209
duclinh <linh1862002@gmail.com>
2024-06-14 16:23:31 +0700

    14-6-2024 register: jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/usersModel");
 const catchAsync = require("../utils/catchAsync");
+const jwt = require("jsonwebtoken");
 // signup 
 exports.signup = catchAsync( async(req, res, next) => {
     const newUser = await User.create({
@@ -8,8 +9,12 @@ exports.signup = catchAsync( async(req, res, next) => {
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm
     });
+    const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
     res.status(201).json({
         status: "Success",
+        token,
         data: {
             user: newUser,
         },
